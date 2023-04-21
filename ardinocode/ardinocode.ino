3e2f15939a2732bb80d908ee410cf436853e1572
Evan <evanmwaite@gmail.com>
2023-04-21 14:18:09 -0500

    added comments

--- ardinocode/ardinocode.ino
+++ ardinocode/ardinocode.ino
@@ -1,37 +1,32 @@
-void setup() {
-  // put your setup code here, to run once:
+/* This code was written for Northwestern University's 2023 IEEE Showcase
+// The NUMeter is a device that uses pH, TDS, and turbidity to determine
+whether a water sample is safe to drink */
+
+void setup()
+{
   Serial.begin(115200);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  int sensorValue = analogRead(A1);
+void loop()
+{
+  int turbidityValue = analogRead(A1);
   int tdsValue = analogRead(A3);
   int pH = analogRead(A4);
-  float voltage = tdsValue*5.0/1024.0;
+  float voltage = tdsValue * 5.0 / 1024.0;
   float ppm = 133.42 * voltage * voltage * voltage - 255.86 * voltage * voltage + 857.39 * voltage * 0.5;
-  
-  float caliValue = 21.34-11.9;
+
+  float caliValue = 21.34 - 11.9; // value used to calibrate sensor to known pH test value
   float volt = (float)pH * 5.0 / 1024 / 6;
   float phValue = -5.70 * volt + caliValue;
 
-  /*Serial.println("sensorValue");
-  Serial.println(sensorValue);
-  Serial.println("tdsValue");
-  Serial.println(tdsValue);
-  Serial.println("ppm");
-  Serial.println(ppm);
-
-  Serial.println("pH value");
-  Serial.println(phValue);*/
-
-  if (sensorValue < 614 || phValue > 8.5 || phValue < 6.5 || ppm > 500) {
-    Serial.println ("not safe");
+  if (sensorValue < 614 || phValue > 8.5 || phValue < 6.5 || ppm > 500)
+  {
+    Serial.println("not safe");
   }
-  else {Serial.println("safe");}
-
+  else
+  {
+    Serial.println("safe");
+  }
 
   delay(100);
-
-  
 }
